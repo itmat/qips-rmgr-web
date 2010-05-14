0c865dc0bb49aee4b70fd82406e7c6fe4cac683b
David Austin <daustin@brc044.apng.med.upenn.edu>
2010-05-14 14:19:09 -0400

    Added support for roles lookup via activeresource from wfmgr.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -13,6 +13,16 @@ class RolesController < ApplicationController
     end
   end
 
+  # GET /roles/1
+  # GET /roles/1.xml
+  def show
+    @role = Role.find(params[:id])
+
+    respond_to do |format|
+      format.xml  { render :xml => @role }
+    end
+  end
+
   # GET /roles/new
   # GET /roles/new.xml
   def new
