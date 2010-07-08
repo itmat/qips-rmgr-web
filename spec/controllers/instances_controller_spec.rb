934ae9e955366fefb6badf5df2f69d1016330698
David Austin <daustin@meath.gcrc.upenn.edu>
2010-07-08 13:45:31 -0400

    Fixed issues with instance spec.

--- spec/controllers/instances_controller_spec.rb
+++ spec/controllers/instances_controller_spec.rb
@@ -3,7 +3,7 @@ require 'json'
 
 describe InstancesController do
   integrate_views
-
+  
   default_message = {
     :instance_id => 'i-abcd1234',
     :state => 'busy',
@@ -18,13 +18,26 @@ describe InstancesController do
     :timeout => 60,
     :ruby_pid => 7890,
     :child_procs => ['test1', 'test2']
-       }
+  }
+  
+  before(:all) do
+
+  end
+
+  after(:all) do
+
+    Instance.find(:all).each do |i|
+      
+      i.terminate
+      sleep 10
+      
+    end
+  end
 
   describe "GET set_status" do 
     it "should fill in the vars of an instance during set status message" do
       instance = Factory(:instance, :instance_id => 'i-abcd1234')
       get :set_status, :message => default_message.to_json
-      # Delayed::Worker.new.work_off
       instance.reload 
       instance.state.should == 'busy'
       instance.ruby_cpu_usage.should == 12.34
@@ -47,18 +60,16 @@ describe InstancesController do
     it "should NOT send KILL response normally." do
       instance = Factory(:instance, :instance_id => 'i-abcd1234')
       get :set_status, :message => default_message.to_json
-      # Delayed::Worker.new.work_off
       response.body.should_not match /KILL/
     end
   end
 
   describe "GET set_status" do 
     it "should send KILL response if ruby times out.  Also increments ruby cycle count." do
-      custom_message = default_message
+      custom_message = default_message.clone
       custom_message[:timestamp] = "#{(Time.now - 4000)}"
       instance = Factory(:instance, :instance_id => 'i-abcd1234')
       get :set_status, :message => custom_message.to_json
-      # Delayed::Worker.new.work_off
       instance.reload
       instance.ruby_cycle_count.should == 1
       response.body.should match /KILL/
@@ -67,7 +78,7 @@ describe InstancesController do
 
   describe "GET set_status" do 
     it "send KILL response if ruby times out, but recycles node when cycle count > MAX." do
-      custom_message = default_message
+      custom_message = default_message.clone
       custom_message[:timestamp] = "#{Time.now - 4000}"
       instance = Factory(:instance, :instance_id => 'i-abcd1234', :ruby_cycle_count => (RUBY_CYCLE_MAX + 1))
       get :set_status, :message => custom_message.to_json
@@ -82,11 +93,10 @@ describe InstancesController do
   
   describe "GET set_status" do 
     it "send KILL response if ruby times out, but not when cycle count > MAX. should shut down a node that exceeds MAX CYCLE" do
-      custom_message = default_message
+      custom_message = default_message.clone
       custom_message[:timestamp] = "#{Time.now - 4000}"
       instance = Factory(:instance, :instance_id => 'i-abcd1234', :cycle_count => (NODE_CYCLE_MAX + 1), :ruby_cycle_count => (RUBY_CYCLE_MAX + 1))
       get :set_status, :message => custom_message.to_json
-      # Delayed::Worker.new.work_off
       instance.reload
       instance.state.should == 'shutdown'
 
@@ -95,7 +105,7 @@ describe InstancesController do
   
   describe "GET set_status" do 
     it "should shutdown node if it reports an error." do
-      custom_message = default_message
+      custom_message = default_message.clone
       custom_message[:state] = "error"
       custom_message[:error_message] = "test error message"
       instance = Factory(:instance)
@@ -107,13 +117,13 @@ describe InstancesController do
       get :set_status, :message => custom_message.to_json
       instance.reload
       instance.state.should == 'shutdown'
-
+      
     end
   end
   
   describe "GET set_status" do 
     it "should NOT shutdown node if it reports an error and is an admin instance." do
-      custom_message = default_message
+      custom_message = default_message.clone
       custom_message[:state] = "error"
       custom_message[:error_message] = "test error message"
       admin_farm = Factory(:farm, :farm_type => 'admin')
@@ -128,7 +138,9 @@ describe InstancesController do
       sleep 5
       instance.reload
       instance.state.should_not == 'shutdown'
-
+      instance.terminate #clean up
+      sleep 3
+      
     end
   end
   
